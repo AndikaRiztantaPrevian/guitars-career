17957b389de851a20c1cf5e02e2931c8e4e302cd
Yapidev <88489723+Yapidev@users.noreply.github.com>
2024-05-31 12:32:17 +0700

    pindahin ke livewire components

---
+++ app/Livewire/User/Pricing/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\User\Pricing;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.pricing.index');
+    }
+}
